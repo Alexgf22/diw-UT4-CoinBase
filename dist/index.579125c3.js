7504b10cd78e9ad1e8d32ea996a3197a73f92473
Alexgf22 <agarcia@iesrafaelalberti.es>
2022-12-09 21:30:49 +0100

    cambios modo dark

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -1,32 +1,11 @@
-/*const dark = document.querySelector('#pie-pagina__modo-oscuro');
-const body = document.querySelector('body');
-
-load();
-
-dark.addEventListener('click', e => {
-    body.classList.toggle('theme--dark');
-    store(body.classList.contains('theme--dark'));
-});
-
-function load() {
-    const darkmode = localStorage.getItem('theme--dark');
-
-    if(!darkmode) {
-        store('false');
-     } else if(darkmode == 'true') {
-        body.classList.add('theme--dark');
-     }
-    }
-
-    function store(value) {
-        localStorage.setItem('theme--dark',value);
-    }
-    */ function themeDark() {
+function themeDarkContacto() {
     // Página contacto
     var fieldsetContacto = document.getElementById("formulario-contacto__fieldset--darkmode");
     fieldsetContacto.classList.toggle("formulario-contacto__fieldset--oscuro");
     var mainContacto = document.getElementById("main-pagina-contacto--darkmode");
     mainContacto.classList.toggle("main-pagina-contacto--oscuro");
+}
+function themeDarkInicioSesion() {
     // Página inicio sesión
     var fieldsetInicioSesion = document.getElementById("form-inicio-sesion__fieldset--darkmode");
     fieldsetInicioSesion.classList.toggle("form-inicio-sesion__fieldset--oscuro");
