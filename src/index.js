f5fa138a747a72f0c1e73e8da2163160f0c1f87f
Alexgf22 <agarcia@iesrafaelalberti.es>
2022-12-09 18:05:15 +0100

    probando para sacar darkmode

---
+++ src/index.js
@@ -0,0 +1,34 @@
+/*const dark = document.querySelector('#pie-pagina__modo-oscuro');
+const body = document.querySelector('body');
+
+load();
+
+dark.addEventListener('click', e => {
+    body.classList.toggle('theme--dark');
+    store(body.classList.contains('theme--dark'));
+});
+
+function load() {
+    const darkmode = localStorage.getItem('theme--dark');
+
+    if(!darkmode) {
+        store('false');
+     } else if(darkmode == 'true') {
+        body.classList.add('theme--dark');
+     }
+    }
+
+    function store(value) {
+        localStorage.setItem('theme--dark',value);
+    }
+    */
+
+
+function darkMode(){
+    var elementb = document.getElementById("#tema-claro-body");
+    elementb.classList.toggle("body-dark-mode");
+
+    //var elementh = document.getElementById("bot-img-darkMode");
+    //elementh.classList.toggle("boton-generar__bot-img-darkMode");
+}
+    
